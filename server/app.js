41a25f655dbfae61aa3437a254719d1351303b27
Andre Pires <andre.rua.pires@gmail.com>
2019-08-03 14:57:13 +0100

    tutorial #28
    Added frontend in React, and using apollo to communicate with graphql server. Added some components

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin request
+app.use(cors());
 
 mongoose.connect('mongodb+srv://testuser:test@gql-db-rskwf.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
 mongoose.connection.once('open', () => {
